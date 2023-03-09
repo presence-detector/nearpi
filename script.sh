d5218b9f10e89d09a77186f2698585bf10aa6466
Ivan Comi <46270073+Ivnoo@users.noreply.github.com>
2023-03-09 11:50:52 +0100

    script.sh | Rilevazione potenza e invio JSON a master

---
+++ script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+macAddr=2C:BE:EB:17:CB:B3;
+timeStamp=`date`
+
+while sleep 1;
+do
+        power=`hcitool rssi $macAddr | cut -d " " -f4`;
+        #echo $HOSTNAME | tr '\n' ';' &&  echo $macAddr | tr '\n' ';' && hcitool rssi $macAddr | cut -d " " -f4;
+
+        if [ $power -lt -5 ]
+        then
+                JSON_STRING=$( jq -n \
+                  --arg ho "$HOSTNAME" \
+                  --arg ma "$macAddr" \
+                  --arg pw "$power" \
+                  --arg ts "$timeStamp" \
+                  '{location: $ho, macAddress: $ma, power: $pw, timeStamp: $ts}' )
+                #echo "Si sta allontanando";
+                curl -X POST http://192.168.0.206:1880/test/#flow/da4824d9d2cc87dc -H 'Content-Type: application/json' -d "$JSON_STRING";
+        fi
+done
+
+
+
+
+
+
+
+
+
