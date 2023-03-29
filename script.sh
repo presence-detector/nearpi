4c4a2fe268a68ef8318803a62bddda4214c203ac
Ivan Comi <46270073+Ivnoo@users.noreply.github.com>
2023-03-29 09:13:42 +0200

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
+#macAddr=2C:BE:EB:17:CB:B3;
 macAddr=$1;
-timeStamp=`date`
-
+timeStamp=`date`;
+pi=`hciconfig | grep "BD Address:" | cut -d " " -f 3`
 while sleep 1;
 do
         power=`hcitool rssi $macAddr | cut -d " " -f4`;
-        #echo $HOSTNAME | tr '\n' ';' &&  echo $macAddr | tr '\n' ';' && hcitool rssi $macAddr | cut -d " " -f4;
-
-        if [ $power -lt -5 ]
+        echo $HOSTNAME | tr '\n' ';' &&  echo $macAddr | tr '\n' ';' && hcitool rssi $macAddr | cut -d " " -f4;
+        re='^[0-9]+$';
+        if ! [[ $power =~ $re ]]
         then
+                p=99
+                power=$((p))
                 JSON_STRING=$( jq -n \
-                  --arg ho "$HOSTNAME" \
-                  --arg ma "$macAddr" \
-                  --arg pw "$power" \
-                  --arg ts "$timeStamp" \
-                  '{location: $ho, macAddress: $ma, power: $pw, timeStamp: $ts}' )
+                --arg ho "$HOSTNAME" \
+                --arg ma "$macAddr" \
+                --arg pi "$pi" \
+                --arg pw "$power" \
+                --arg ts "$timeStamp" \
+                '{location: $ho, macAddressPhone: $ma, macAddressPi: $pi, power: $pw, timeStamp: $ts}' )
+                echo "Si sta allontanando";
+                curl -X POST http://192.168.0.2:1880/test/#flow/da4824d9d2cc87dc -H 'Content-Type: application/json' -d>
+        else
+                JSON_STRING=$( jq -n \
+                --arg ho "$HOSTNAME" \
+                --arg ma "$macAddr" \
+                --arg pi "$pi" \
+                --arg pw "$power" \
+                --arg ts "$timeStamp" \
+                '{location: $ho, macAddressPhone: $ma, macAddressPi: $pi, power: $pw, timeStamp: $ts}' )
                 #echo "Si sta allontanando";
-                curl -X POST http://192.168.0.2:1880/test/#flow/da4824d9d2cc87dc -H 'Content-Type: application/json' -d "$JSON_STRING";
-        fi
+                curl -X POST http://192.168.0.2:1880/test/#flow/da4824d9d2cc87dc -H 'Content-Type: application/json' -d>
+        fi                                                                                                              
 done
-
-
-
-
-
-
-
-
-
